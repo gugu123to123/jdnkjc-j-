0dd403e730dde8dc94df73c078986b3f3f4414de
gugu123to123 <98198154+gugu123to123@users.noreply.github.com>
2023-04-11 19:59:56 -0600

    Add files via upload

---
+++ clase 170.py
@@ -0,0 +1,39 @@
+class History_in_space():
+    def year_2008(self):
+        print("\n","eventos ocurridos en 2008","\n")
+        print ("India lanzó Chandrayaan con éxito en 2008 para explorar la luna. La nave se insert")           
+
+    def year_2011(self):
+        print("\n","Evento ocurrido en el campo del espacio en 20ó en la órbita lunar en su primer intento. Uno de los mayores logros de Chadrayaan fue el descubrimiento de las moléculas de agua en el suelo lunar.")
+
+
+class History_in_medicina():
+    def year_2008(self):
+        print("\n","eventos ocurridos en 2008","\n")
+        print("Aunque los científicos han descubierto durante mucho tiempo cómo se transmite la malaria y saben cómo prevenirla, casi 1 millón de personas mueren cada año a causa de la enfermedad, según estimaciones internacionales. Finalmente, el 8 de diciembre, los primeros resultados de una vacuna contra la malaria que se muestra prometedora llegaron a noticias.")
+        
+    def year_2011(self):
+        print("\n","Evento ocurrido en el campo del espacio en 20ó en la órbita lunar en su primer intento. Uno de los mayores logros de Chadrayaan fue el descubrimiento de las moléculas de agua en el suelo lunar.")
+
+
+class History_in_sports():
+    def year_2008(self):
+        print("\n","eventos ocurridos en 2008","\n")
+        print("En 2008, Abhinav Bindra hizo historia al ganar el oro en el evento masculino de rifle de aire de 10 m en los Juegos de Beijing. Su oro sigue siendo la primera y única medalla de oro individual del país hasta la fecha.")
+        
+    def year_2011(self):
+        print("\n","Evento ocurrido en el campo del espacio en 20ó en la órbita lunar en su primer intento. Uno de los mayores logros de Chadrayaan fue el descubrimiento de las moléculas de agua en el suelo lunar.")
+
+
+
+space = History_in_space()
+space.year_2008()
+space.year_2011()
+
+medical = History_in_medicina()
+medical.year_2008()
+medical.year_2011()
+
+sports = History_in_sports()
+sports.year_2008()
+sports.year_2011()
